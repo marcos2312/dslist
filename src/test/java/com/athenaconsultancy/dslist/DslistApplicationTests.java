a8ea8728834ae11341889718a0477e09bea5b8ba
marcos2312 <marcos.rcomercial@gmail.com>
2023-05-09 16:06:53 -0300

    Project created

---
+++ src/test/java/com/athenaconsultancy/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athenaconsultancy.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
