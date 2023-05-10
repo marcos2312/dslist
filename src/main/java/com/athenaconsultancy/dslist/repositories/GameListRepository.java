47527c5888673754a8eac2462c1b109a7e48e1d7
marcos2312 <marcos.rcomercial@gmail.com>
2023-05-10 16:07:56 -0300

    GET lists

---
+++ src/main/java/com/athenaconsultancy/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.athenaconsultancy.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.athenaconsultancy.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
